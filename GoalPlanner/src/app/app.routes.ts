af68c1c5f74928ec78ba51cfaf8c6e2acd8e1e08
santoshdev4456 <nsantosh.dev@gmail.com>
2025-03-25 14:50:31 +0530

    2nd commit

--- GoalPlanner/src/app/app.routes.ts
+++ GoalPlanner/src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { GoalListComponent } from './goal-list/goal-list.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { RemindersComponent } from './reminders/reminders.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path:'home',
+        component: HomeComponent
+    },{
+        path: 'dashboard',
+        component: DashboardComponent
+    },
+    {
+        path : 'goals',
+        component: GoalListComponent
+    },{
+        path: 'tasks',
+        component: TaskListComponent
+    },{
+        path: 'reminders',
+        component: RemindersComponent
+    }
+];
